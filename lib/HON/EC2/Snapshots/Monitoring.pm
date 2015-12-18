6f028e9b030d24f21607b5eba661dd245d5ad2a4
William Belle <william.belle@gmail.com>
2015-12-18 11:19:11 +0100

    init with module-starter

---
+++ lib/HON/EC2/Snapshots/Monitoring.pm
@@ -0,0 +1,119 @@
+package HON::EC2::Snapshots::Monitoring;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+HON::EC2::Snapshots::Monitoring - The great new HON::EC2::Snapshots::Monitoring!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use HON::EC2::Snapshots::Monitoring;
+
+    my $foo = HON::EC2::Snapshots::Monitoring->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+William Belle, C<< <william.belle at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-hon-ec2-snapshots-monitoring at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=HON-EC2-Snapshots-Monitoring>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc HON::EC2::Snapshots::Monitoring
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=HON-EC2-Snapshots-Monitoring>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/HON-EC2-Snapshots-Monitoring>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/HON-EC2-Snapshots-Monitoring>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/HON-EC2-Snapshots-Monitoring/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 William Belle.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 dated June, 1991 or at your option
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+A copy of the GNU General Public License is available in the source tree;
+if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+
+=cut
+
+1; # End of HON::EC2::Snapshots::Monitoring
